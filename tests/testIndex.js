a3f53000cc79e192fc50b40f9a62825228074828
olagunju gbolahan <gbolahanpaul@gmail.com>
2018-08-13 08:38:02 +0100

    test added for api/v1/questions

--- tests/testIndex.js
+++ tests/testIndex.js
@@ -2,8 +2,8 @@ import chai from "chai";
 import chaiHttp from "chai-http";
 
 import app from "../src/index";
-const  should = chai.should();
 
+const should = chai.should();
 
 chai.use(chaiHttp);
 
@@ -14,6 +14,18 @@ describe("/Home Directory", () => {
       .get("/")
       .end((err, res) => {
         res.should.have.status(200);
+        done();
+      });
+  });
+
+  it("it send an ok status and be an array with 3 items", done => {
+    chai
+      .request(app)
+      .get("/api/v1/questions")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("array");
+        res.body.length.should.be.eql(3);
         done();
       });
   });
