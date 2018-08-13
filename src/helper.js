2abcd38da92839b35a509d4f8f2dff2f91d6fde6
gbols <gbolahanpaul@gmail.com>
2018-08-13 20:18:02 +0100

    Revert "Post ans"

--- src/helper.js
+++
@@ -1,9 +0,0 @@
-import data from "./model";
-
-const validateId = (id) => data.find(que => que.id === id);
-
-const validPost = (input) => {
-  if (typeof input === "string" && input.length > 10) return true
-}
-
-export {validateId , validPost}
