451cc24e7c66e2fc083707962d1fade703ffd86f
olagunju gbolahan <gbolahanpaul@gmail.com>
2018-08-21 09:21:18 +0100

    test suites aren't working as expected[finshes 159915657]

--- tests/testIndex.js
+++ tests/testIndex.js
@@ -8,40 +8,40 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe("/Home Directory", () => {
-  // it("it send an ok status and be an array with 3 items", done => {
-  //   chai
-  //     .request(app)
-  //     .get("/api/v1/questions")
-  //     .end((err, res) => {
-  //       res.should.have.status(200);
-  //       res.body.should.be.a("array");
-  //       res.body.length.should.be.eql(3);
-  //       done();
-  //     });
-  // });
-
-  it("it send an ok status and be an object", done => {
+  it("it send an ok status and be an array with 3 items", done => {
     chai
       .request(app)
-      .get("/api/v1/question/2")
-      .set("Content-Type", "application/json")
-      .set("Accept", "application/json")
+      .get("/api/v1/questions")
       .end((err, res) => {
         res.should.have.status(200);
-        res.body.should.be.a("object");
+        res.body.should.be.a("array");
+        res.body.length.should.be.eql(3);
         done();
       });
   });
 
-  // it("it should be an error", done => {
+  // it("it send an ok status and be an object", done => {
   //   chai
   //     .request(app)
-  //     .get("/api/v1/questions/4")
+  //     .get("/api/v1/question/2")
+  //     .set("Content-Type", "application/json")
+  //     .set("Accept", "application/json")
   //     .end((err, res) => {
-  //       res.should.have.status(404);
+  //       res.should.have.status(200);
+  //       res.body.should.be.a("object");
   //       done();
   //     });
   // });
+
+  it("it should be an error", done => {
+    chai
+      .request(app)
+      .get("/api/v1/questions/4")
+      .end((err, res) => {
+        res.should.have.status(404);
+        done();
+      });
+  });
 });
 
 // describe("/POST request", () => {
