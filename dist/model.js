e1f8b3a75ca90137b8d235d1ce0b9bf98c3f0c16
olagunju gbolahan <gbolahanpaul@gmail.com>
2018-08-21 14:34:10 +0100

    modified database so each answer has a unique id

--- dist/model.js
+++ dist/model.js
@@ -6,15 +6,15 @@ Object.defineProperty(exports, "__esModule", {
 var data = [{
   id: 1,
   question: "what is superman's real name",
-  answers: [{ vote: 0, response: "clark kent", accept: false }, { vote: 0, response: "bruce wayne", accept: false }, { vote: 0, response: "dianna prince", accept: false }]
+  answers: [{ id: 1, vote: 0, response: "clark kent", accept: false }, { id: 2, vote: 0, response: "bruce wayne", accept: false }, { id: 3, vote: 0, response: "dianna prince", accept: false }]
 }, {
   id: 2,
   question: "what is batman's real name",
-  answers: [{ vote: 0, response: "lois lane", accept: false }, { vote: 0, response: "bruce wayne", accept: false }, { vote: 0, response: "peter parker", accept: false }]
+  answers: [{ id: 1, vote: 0, response: "lois lane", accept: false }, { id: 2, vote: 0, response: "bruce wayne", accept: false }, { id: 3, vote: 0, response: "peter parker", accept: false }]
 }, {
   id: 3,
   question: "what is wonder woman's real name",
-  answers: [{ vote: 0, response: "tony stark", accept: false }, { vote: 0, response: "dwanye johnson", accept: false }, { vote: 0, response: "dianna prince", accept: false }]
+  answers: [{ id: 1, vote: 0, response: "tony stark", accept: false }, { id: 2, vote: 0, response: "dwanye johnson", accept: false }, { id: 3, vote: 0, response: "dianna prince", accept: false }]
 }];
 
 exports.default = data;
--- src/model.js
+++ src/model.js
@@ -3,27 +3,27 @@ const data = [
     id: 1,
     question: "what is superman's real name",
     answers: [
-      { vote: 0, response: "clark kent", accept: false },
-      { vote: 0, response: "bruce wayne", accept: false },
-      { vote: 0, response: "dianna prince", accept: false }
+      { id: 1, vote: 0, response: "clark kent", accept: false },
+      { id: 2, vote: 0, response: "bruce wayne", accept: false },
+      { id: 3, vote: 0, response: "dianna prince", accept: false }
     ]
   },
   {
     id: 2,
     question: "what is batman's real name",
     answers: [
-      { vote: 0, response: "lois lane", accept: false },
-      { vote: 0, response: "bruce wayne", accept: false },
-      { vote: 0, response: "peter parker", accept: false }
+      { id: 1, vote: 0, response: "lois lane", accept: false },
+      { id: 2, vote: 0, response: "bruce wayne", accept: false },
+      { id: 3, vote: 0, response: "peter parker", accept: false }
     ]
   },
   {
     id: 3,
     question: "what is wonder woman's real name",
     answers: [
-      { vote: 0, response: "tony stark", accept: false },
-      { vote: 0, response: "dwanye johnson", accept: false },
-      { vote: 0, response: "dianna prince", accept: false }
+      { id: 1, vote: 0, response: "tony stark", accept: false },
+      { id: 2, vote: 0, response: "dwanye johnson", accept: false },
+      { id: 3, vote: 0, response: "dianna prince", accept: false }
     ]
   }
 ];
