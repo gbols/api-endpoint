eae1c0a97aed1ffcc2dc9dd5e522ef04c12c55ec
olagunju gbolahan <gbolahanpaul@gmail.com>
2018-08-29 10:09:45 +0100

    testing

--- src/index.js
+++ src/index.js
@@ -1,50 +1,40 @@
-import express from "express";
-import data from "./model";
-import { validateId } from "./helper";
+const form = document.forms.signup;
+const password = forms.password;
+const email = forms.email;
+const confirmPassword = forms.confirm;
+const username = forms.username;
 
-const app = express();
+function validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 
-app.use(express.json());
+const validEmail = validateEmail(email);
 
-app.get("/", (req, res) => {
-  res.send("use the following URI /api/v1/questions");
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  if (!validEmail) {
+    alert("The email provided is invalid");
+  }
+
+  if (password !== confirmPassword) {
+    alert("The passwords provided do not match");
+  }
+
+  const url = "https://thegbols.herokuapp.com/auth/signup";
+  const headers = new Headers({
+    "Content-Type": "apllication/json",
+    "Accept-Charset": "utf-8"
+  });
+
+  const request = new Request(url, {
+    method:'POST'
+  },{
+    headers
+  });
+
+  fetch(request)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
 });
-
-app.get("/api/v1/questions", (req, res) => {
-  res.send(data);
-});
-
-app.get("/api/v1/questions/:id", (req, res) => {
-  const que = validateId(parseInt(req.params.id));
-  if (!que)
-    return res.status(404).send("The question with the given ID was not found");
-  res.send(que);
-});
-
-app.post("/api/v1/questions", (req, res) => {
-  const question = {
-    id: data.length + 1,
-    question: req.body.question,
-    answers: []
-  };
-  data.push(question);
-  res.send(question);
-});
-
-app.post("/api/v1/questions/:id/answers", (req, res) => {
-  const que = validateId(parseInt(req.params.id));
-  if (!que)
-    return res.status(404).send("The question with the given ID was not found");
-  const ans = {
-    vote: 0,
-    response: req.body.answer
-  };
-  que.answers.push(ans);
-  res.send(que);
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port);
-console.log("listening on port 3000 ......");
-
-export default app;
